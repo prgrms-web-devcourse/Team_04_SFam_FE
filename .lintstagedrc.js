a2ea3cea70d2295eb2a84047dffaa11c7f927f3c
chmini <papa00103@naver.com>
2022-07-20 18:55:07 +0900

    Initial Setting

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+const buildTypeCheckCommand = () => "npx tsc --noEmit";
+
+module.exports = {
+  "*.{ts,tsx}": [buildTypeCheckCommand, buildEslintCommand],
+  "*.{ts,tsx,css}": "prettier --write",
+};
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: rgb(240, 248, 255);
+`;
+
+function HomePage() {
+  return <Box>Welcome to Next.js!</Box>;
+}
+
+export default HomePage;
