5d039d9b594755b43e7abe9b17845134498fbc0f
chmini <papa00103@naver.com>
2022-07-29 00:55:25 +0900

    feat: useAxios 훅 추가

---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+import { axiosDefaultInstance } from '@api/axiosInstances';
+
+const useAxios = (axiosConfig: AxiosRequestConfig, axiosInstance: AxiosInstance = axiosDefaultInstance) => {
+  const [response, setResponse] = React.useState<AxiosResponse>();
+  const [error, setError] = React.useState<AxiosError>();
+  const [loading, setLoading] = React.useState(true);
+
+  const request = React.useCallback(async () => {
+    try {
+      const result = await axiosInstance(axiosConfig);
+      setResponse(result);
+    } catch (err) {
+      setError(err as AxiosError);
+    } finally {
+      setLoading(false);
+    }
+  }, [axiosConfig, axiosInstance]);
+
+  React.useEffect(() => {
+    request();
+  }, []);
+
+  return { response, error, loading, request };
+};
+
+export default useAxios;
