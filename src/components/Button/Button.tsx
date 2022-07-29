2f57d8d9e1abb7ddd166d2c23d562988483bdb51
qq8721443 <qq8721443@naver.com>
2022-07-29 16:02:05 +0900

    refactor: 속성값 문자열로 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,50 +3,35 @@ import ButtonContainer from './Button.style';
 
 interface Props {
   color?: string;
-  width?: string | number;
-  height?: string | number;
+  width?: string;
+  height?: string;
   children?: string;
   round?: boolean;
-  fontSize?: string | number;
+  fontSize?: string;
   onClick?: ComponentProps<'button'>['onClick'];
 }
 
 const Button = ({
-  color,
-  children,
-  width = 384,
-  height = 50,
+  color = 'primary',
+  width = '384px',
+  height = '50px',
   round = false,
-  fontSize = 20,
+  fontSize = '20px',
   onClick,
+  children,
   ...props
-}: Props) => {
-  let replacedWidth = '';
-  let replacedHeight = '';
-  let replacedFontSize = '';
-  if (typeof width === 'number') {
-    replacedWidth = `${width}px`;
-  }
-  if (typeof height === 'number') {
-    replacedHeight = `${height}px`;
-  }
-  if (typeof fontSize === 'number') {
-    replacedFontSize = `${fontSize}px`;
-  }
-
-  return (
-    <ButtonContainer
-      color={color}
-      width={replacedWidth}
-      height={replacedHeight}
-      round={round}
-      fontSize={replacedFontSize}
-      onClick={onClick}
-      {...props}
-    >
-      {children}
-    </ButtonContainer>
-  );
-};
+}: Props) => (
+  <ButtonContainer
+    color={color}
+    width={width}
+    height={height}
+    round={round}
+    fontSize={fontSize}
+    onClick={onClick}
+    {...props}
+  >
+    {children}
+  </ButtonContainer>
+);
 
 export default Button;
