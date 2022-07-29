1f987de4d15e740acbea17b83646377a3805c645
dustmddus <dustmddus5@gmail.com>
2022-07-29 18:01:48 +0900

    feat: sportsButton 컴포넌트 구현

---
+++ src/components/SportsButton/SportsButton.style.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import styled from '@emotion/styled';
+
+export const Container = styled.button`
+  position: relative;
+  display: inline-block;
+  border: none;
+  border-radius: 50%;
+`;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { Divider } from './Divider';
 export { Badge } from './Badge';
 export { Input } from './Input';
 export { Avatar } from './Avatar';
+export { SportsButton } from './SportsButton';
