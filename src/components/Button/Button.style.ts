ad4c127ed179f95603f74421fe8badcaec2f3d09
qq8721443 <qq8721443@naver.com>
2022-07-29 16:07:07 +0900

    refactor: styled component round 처리 방식 수정

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -1,34 +1,36 @@
 import styled from '@emotion/styled';
 
 interface Props {
-  color: string | undefined;
+  color: string;
   width: string;
   height: string;
   round: boolean;
   fontSize: string;
 }
 
-const ButtonContainer = styled.button<Props>(
-  {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    border: 'none',
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-  ({ width, height, round, color, fontSize }) => ({
-    width,
-    height,
-    borderRadius: round
-      ? `${parseInt(height.replace('px', ''), 10) / 2}px`
-      : '8px',
-    backgroundColor: color === 'primary' ? '#1fab89' : '#f19a78',
-    fontSize,
-    '&:active': {
-      backgroundColor: color === 'primary' ? '#178a6e' : '#ed794c',
-    },
-  }),
-);
+const ButtonContainer = styled.button<Props>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  border: none;
+  border-radius: ${({ round, height }) =>
+    // eslint-disable-next-line no-nested-ternary
+    round
+      ? height?.includes('px')
+        ? `${parseInt(height?.replace('px', ''), 10) / 2}px`
+        : `${parseInt(height?.replace('rem', ''), 10) / 2}rem`
+      : '8px'};
+  background-color: ${({ color }) =>
+    color === 'primary' ? '#1fab89' : '#f19a78'};
+  font-size: ${({ fontSize }) => fontSize};
+  font-weight: bold;
+  color: #fff;
+  &:active {
+    background-color: ${({ color }) =>
+      color === 'primary' ? '#178a6e' : '#ed794c'};
+  }
+`;
 
 export default ButtonContainer;
