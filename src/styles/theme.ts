41adba0a715e23cbb397eaee0654fcaae2e4cc2d
chmini <papa00103@naver.com>
2022-07-29 23:47:07 +0900

    feat: 테마 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+import { Theme } from '@emotion/react';
+
+const theme: Theme = {
+  color: {
+    primary: '#F19A78',
+    secondary: '#1FAB89',
+    background: '#fff',
+    yellow: '#FDCF65',
+    gray800: '#212529',
+    gray700: '#343A40',
+    gray600: '#495057',
+    gray500: '#ADB5BD',
+    gray400: '#CED4DA',
+    gray300: '#CED4DA',
+    gray200: '#E9ECEF',
+    gray100: '#F8F9FA',
+    green200: '#62D2A2',
+    green100: '#D7FBE8',
+  },
+  fontSize: {
+    h1: '24px',
+    b1: '20px',
+    b2: '18px',
+    b3: '16px',
+    c1: '12px',
+    c2: '11px',
+  },
+  borderRadius: '8px',
+};
+
+export default theme;
