179e8bcd8c5f91ce9247db2ccc0740f43831e857
chmini <papa00103@naver.com>
2022-07-29 23:46:33 +0900

    feat: 폰트 추가 및 글로벌 스타일 추가

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react';
+
+import reset from './reset';
+
+const globalStyles = css`
+  ${reset}
+
+  body {
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+      'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+      sans-serif;
+  }
+`;
+
+export default globalStyles;
