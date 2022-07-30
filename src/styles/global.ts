4aa562875889e01d9dfdc776453c772843a0f032
chmini <papa00103@naver.com>
2022-07-30 16:26:28 +0900

    feat: box-sizing border-box 추가

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,16 @@ import reset from './reset';
 const globalStyles = css`
   ${reset}
 
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
   body {
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
       'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
