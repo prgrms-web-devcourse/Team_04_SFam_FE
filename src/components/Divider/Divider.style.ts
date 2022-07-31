7e9958164205782185ae718f9a8e48c00b704434
dustmddus <dustmddus5@gmail.com>
2022-07-31 18:45:14 +0900

    refactor: Divider 컴포넌트 수정

--- src/components/Divider/Divider.style.ts
+++ src/components/Divider/Divider.style.ts
@@ -1,8 +1,15 @@
 import styled from '@emotion/styled';
 
-export const Line = styled.hr`
+interface Props {
+  width?: string;
+}
+
+const Divider = styled.hr<Props>`
   display: block;
   height: 1px;
   border: 0;
   background-color: #ced4da;
+  width: ${({ width }) => width};
 `;
+
+export default Divider;
