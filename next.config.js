b1374221275d4fb4b85744ea0a57f1d17ea0acda
Kyubum Jang <jkb2221@gmail.com>
2022-08-01 00:39:11 +0900

    chore: svg import 설정, custom.d.ts 추가, next.config.js webpack 설정 추가

--- next.config.js
+++ next.config.js
@@ -4,8 +4,16 @@
 
 const nextConfig = {
   images: {
-    // TODO: domain 변경 필요
+    // TODO: domains 변경 필요
     domains: ['images.unsplash.com'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+
+    return config;
   },
 };
 
---
+++ src/types/custom.d.ts
@@ -0,0 +1,7 @@
+declare module '*.svg' {
+  import React from 'react';
+
+  const svg: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
