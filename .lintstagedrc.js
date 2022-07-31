c75cde7208b1fae064ed6e1409f450f8abf715f2
Kyubum Jang <jkb2221@gmail.com>
2022-08-01 01:05:53 +0900

    refactor: prettier 적용

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
-const buildTypeCheckCommand = () => "npx tsc --noEmit";
+const buildTypeCheckCommand = () => 'npx tsc --noEmit';
 
 module.exports = {
-  "*.{ts,tsx}": [buildTypeCheckCommand, buildEslintCommand],
-  "*.{ts,tsx,css}": "prettier --write",
+  '*.{ts,tsx}': [buildTypeCheckCommand, buildEslintCommand],
+  '*.{ts,tsx,css}': 'prettier --write',
 };
