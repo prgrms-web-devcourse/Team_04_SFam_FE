593c051c1e2f4b700e33d1ec3c85f6c6f75a7400
chmini <papa00103@naver.com>
2022-08-01 22:45:10 +0900

    feat: 팀 생성페이지 추가 및 팀 생성 폼 구현

--- src/pages/team/create.tsx
+++ src/pages/team/create.tsx
@@ -1,33 +1,7 @@
-import { axiosAuthInstance } from '@api/axiosInstances';
 import { NextPage } from 'next';
 
-const TeamCreatePage: NextPage = () => {
-  const handleClick = () => {
-    const createTeam = async () => {
-      const res = await axiosAuthInstance({
-        method: 'POST',
-        url: '/api/teams',
-        data: {
-          name: '팀 테스트 1',
-          description: '팀 테스트 1 소개',
-          sportsCategory: 'BADMINTON',
-        },
-      });
+import { TeamForm } from '@components/TeamForm';
 
-      console.log(res);
-    };
-
-    createTeam();
-  };
-
-  return (
-    <button
-      type='button'
-      onClick={handleClick}
-    >
-      팀 생성
-    </button>
-  );
-};
+const TeamCreatePage: NextPage = () => <TeamForm />;
 
 export default TeamCreatePage;
