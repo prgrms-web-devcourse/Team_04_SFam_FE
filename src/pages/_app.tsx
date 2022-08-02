8e728cfb9a47dc6b414377f8d06cca22e5919413
chmini <papa00103@naver.com>
2022-08-02 12:31:11 +0900

    feat: 컴포넌트 구현( Layout )

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app';
 import { Global, ThemeProvider } from '@emotion/react';
 
+import { Layout } from '@components/Layout';
+
 import globalStyles from '@styles/global';
 import theme from '@styles/theme';
 import { RecoilRoot } from 'recoil';
@@ -9,7 +11,9 @@ const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
     <Global styles={globalStyles} />
     <RecoilRoot>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </RecoilRoot>
   </ThemeProvider>
 );
