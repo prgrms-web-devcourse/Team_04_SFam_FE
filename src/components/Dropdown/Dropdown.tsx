5bb8c22431bac3c6ea0159fd9ee9bc0769be18ca
chmini <papa00103@naver.com>
2022-08-02 16:19:14 +0900

    feat: 컴포넌트 기능 추가( 외부 영역 클릭 시 드롭다운 닫힘 )

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -16,30 +16,48 @@ const Div = styled.div``;
 const Li = styled.li``;
 
 const Dropdown = ({ dropdownItems, onSelect }: Props) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+
   const [isOpen, setIsOpen] = React.useState(false);
   const [selectedItem, setSelectedItem] = React.useState('');
 
-  const handleToggle = () => {
+  const handleClickToggle = () => {
     setIsOpen((prev) => !prev);
   };
 
-  const handleSelect = (e: React.MouseEvent) => {
+  const handleClickSelect = (e: React.MouseEvent) => {
     const target = e.target as HTMLLIElement;
     setIsOpen((prev) => !prev);
     setSelectedItem(() => target.innerText);
     if (onSelect) onSelect(target.dataset.value || '');
   };
 
+  const handleClickOutSide = React.useCallback(
+    (e: MouseEvent) => {
+      if (isOpen && !ref.current?.contains(e.target as HTMLElement)) {
+        setIsOpen(false);
+      }
+    },
+    [isOpen],
+  );
+
+  React.useEffect(() => {
+    document.addEventListener('click', handleClickOutSide);
+    return () => {
+      document.removeEventListener('click', handleClickOutSide);
+    };
+  }, [ref, handleClickOutSide]);
+
   return (
-    <div>
-      <Div onClick={handleToggle}>{selectedItem ? <div>{selectedItem}</div> : <div>선택해주세요.</div>}</Div>
+    <div ref={ref}>
+      <Div onClick={handleClickToggle}>{selectedItem ? <div>{selectedItem}</div> : <div>선택해주세요.</div>}</Div>
       {isOpen ? (
         <ul>
           {dropdownItems.map((dropdownItem) => (
             <Li
               key={dropdownItem.id}
               data-value={dropdownItem.value}
-              onClick={handleSelect}
+              onClick={handleClickSelect}
             >
               {dropdownItem.text}
             </Li>
