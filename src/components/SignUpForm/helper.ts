0959c871fc2ed6c243d2ed2f84e18d2f4a41bb35
qq8721443 <qq8721443@naver.com>
2022-08-03 14:09:20 +0900

    refactor: 입력값 형식 추가

--- src/components/SignUpForm/helper.ts
+++ src/components/SignUpForm/helper.ts
@@ -10,19 +10,19 @@ const validation = ({ username, nickname, password, passwordCheck }: Values) => 
   if (!username) {
     errors.username = '아이디를 입력해주세요.';
   } else if (!regexUsername.test(username)) {
-    errors.username = '아이디 형식에 맞지 않습니다.';
+    errors.username = '아이디 형식에 맞지 않습니다. (영문 소문자, 숫자 6자리 이상 24자리 이하)';
   }
 
   if (!nickname) {
     errors.nickname = '닉네임을 입력해주세요.';
   } else if (!regexNickname.test(nickname)) {
-    errors.nickname = '닉네임 형식에 맞지 않습니다.';
+    errors.nickname = '닉네임 형식에 맞지 않습니다. (2글자 이상, 16글자 이하)';
   }
 
   if (!password) {
     errors.password = '비밀번호를 입력해주세요.';
   } else if (!regexPassword.test(password)) {
-    errors.password = '비밀번호 형식에 맞지 않습니다.';
+    errors.password = '비밀번호 형식에 맞지 않습니다. (영문 소문자, 대문자, 숫자, 특수문자 포함 8자리 이상)';
   }
 
   if (!passwordCheck) {
