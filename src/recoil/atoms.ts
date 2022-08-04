7569991f1d119f23b8d9560dae0afc72d4c69f6d
qq8721443 <qq8721443@naver.com>
2022-08-04 14:45:16 +0900

    feat: user 전역 상태 추가

---
+++ src/recoil/atoms.ts
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
+
+interface UserState {
+  id?: number;
+  username?: string;
+  nickname?: string;
+}
+
+export const userState = atom<UserState>({
+  key: 'userState',
+  default: {},
+  effects_UNSTABLE: [persistAtom],
+});
