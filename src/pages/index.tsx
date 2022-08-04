75bc1ed3c3134891af390d628fbb9ef706573644
dustmddus <dustmddus5@gmail.com>
2022-08-04 16:41:01 +0900

    feat: 시작페이지 UI 임시 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
-import { MatchList } from '@components/MatchList';
+import SFAM from '@assets/logo/logo.svg';
+import Image from 'next/image';
+import { ColWrapper, B1 } from '@styles/common';
+import { Container } from '@nivo/core';
 
-const Home = () => (
-  <div>
-    <MatchList />
-  </div>
+const Welcome = () => (
+  <Container>
+    <ColWrapper justifyContent='center'>
+      <Image
+        src={SFAM}
+        alt=''
+        width='200px'
+        height='400px'
+      />
+      <B1 font-size='20px'>스포츠를 좋아하는 동네 사람들과</B1>
+      <B1> 팀을 이루고 경기를 매칭해보세요!</B1>
+    </ColWrapper>
+  </Container>
 );
-
-export default Home;
+export default Welcome;
