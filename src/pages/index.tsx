e3d6c823f855dc583885c65ad420c5eea6798a07
dustmddus <dustmddus5@gmail.com>
2022-08-05 03:00:32 +0900

    feat: 시작 페이지 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,8 @@
-import SFAM from '@assets/logo/logo.svg';
-import Image from 'next/image';
-import { ColWrapper, B1 } from '@styles/common';
-import { Container } from '@nivo/core';
+import { Welcome } from '@components/Welcome';
 
-const Welcome = () => (
-  <Container>
-    <ColWrapper justifyContent='center'>
-      <Image
-        src={SFAM}
-        alt=''
-        width='200px'
-        height='400px'
-      />
-      <B1 font-size='20px'>스포츠를 좋아하는 동네 사람들과</B1>
-      <B1> 팀을 이루고 경기를 매칭해보세요!</B1>
-    </ColWrapper>
-  </Container>
+const Start = () => (
+  <div>
+    <Welcome />
+  </div>
 );
-export default Welcome;
+export default Start;
