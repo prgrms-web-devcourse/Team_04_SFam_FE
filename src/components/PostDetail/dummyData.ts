8ced4b1f83010c87be5ceb9b5e60d031aea052d2
dustmddus <dustmddus5@gmail.com>
2022-08-04 15:09:38 +0900

    feat: 공고 상세 UI 구현

---
+++ src/components/PostDetail/dummyData.ts
@@ -0,0 +1,22 @@
+export const dummyData = {
+  data: {
+    title: '역삼역 축구짱 구합니다.',
+    status: '모집 중',
+    author: {
+      id: 1,
+      nickname: '연승연',
+    },
+    sportsCategory: '축구',
+    team: {
+      // 개인전인 경우 null
+      id: 2,
+      name: '우주 최강 연승팀',
+      sportsCategory: '축구',
+    },
+    participants: 8,
+    matchDate: '2022/04/06',
+    matchType: '팀전',
+    content:
+      '안녕하세요. 우주 최강 연승팀입니다. 축구 좀 하시나요. 저는 못해요. 그렇지만 재밌게 놀고 싶습니다. 연락주세요.',
+  },
+};
