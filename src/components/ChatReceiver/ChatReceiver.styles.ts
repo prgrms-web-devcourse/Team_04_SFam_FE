6d8cc6e8a9f8df4ed65e0cae0712b24f198d7e6a
Kyubum Jang <jkb2221@gmail.com>
2022-08-06 03:08:44 +0900

    feat: ChatReceiver 컴포넌트 구현

---
+++ src/components/ChatReceiver/ChatReceiver.styles.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ReceiverWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 130px;
+  padding: 20px;
+  border-radius: 30px;
+  background-color: #e6e6e6;
+`;
+
+export const TimeWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  width: 100px;
+  margin-bottom: 20px;
+`;
