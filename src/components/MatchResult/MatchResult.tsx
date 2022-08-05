fc06b73d7d691e78451382a72a3b29459f0cf633
dustmddus <dustmddus5@gmail.com>
2022-08-06 00:32:06 +0900

    feat: 경기 결과 페이지 구현

--- src/components/MatchResult/MatchResult.tsx
+++ src/components/MatchResult/MatchResult.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -64,5 +65,51 @@ const MatchResult = () => (
   </S.Container>
 );
 >>>>>>> 16cded9 (feat: 경기 결과 페이지 버튼 생성)
+=======
+import { useState } from 'react';
+import theme from '@styles/theme';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import * as S from './MatchResult.styles';
+
+const MatchResult = () => {
+  const [select, setSelect] = useState('');
+  const router = useRouter();
+  const { id } = router.query;
+  const results = [
+    { id: 1, text: '승리' },
+    { id: 2, text: '패배' },
+    { id: 3, text: '무승부' },
+  ];
+  const onClick = (e: React.MouseEvent<HTMLElement>) => {
+    const value = e.target as HTMLElement;
+    const { innerText } = value;
+    if (select === innerText) {
+      setSelect('');
+    } else {
+      setSelect(innerText);
+    }
+  };
+  return (
+    <S.Container>
+      {results.map((result) => (
+        <S.ButtonContainer
+          key={result.id}
+          color={select === result.text ? `${theme.color.primary}` : `${theme.color.gray300}`}
+          onClick={onClick}
+        >
+          {result.text}
+        </S.ButtonContainer>
+      ))}
+      <Link
+        href={`/matches/${id as string}/review`}
+        passHref
+      >
+        <S.SubmitBtn>제출</S.SubmitBtn>
+      </Link>
+    </S.Container>
+  );
+};
+>>>>>>> ab625fa (feat: 경기 결과 페이지 구현)
 
 export default MatchResult;
