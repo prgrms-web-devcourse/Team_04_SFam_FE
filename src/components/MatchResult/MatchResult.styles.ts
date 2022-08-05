a6e17c0c704f68a71a997ec12128d35a295a0e6f
dustmddus <dustmddus5@gmail.com>
2022-08-06 00:32:06 +0900

    feat: 경기 결과 페이지 구현

--- src/components/MatchResult/MatchResult.styles.ts
+++ src/components/MatchResult/MatchResult.styles.ts
@@ -1,5 +1,17 @@
 import styled from '@emotion/styled';
+import { Button } from '@components/Button';
 
 export const Container = styled.div`
   padding: 100px 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+export const ButtonContainer = styled(Button)`
+  margin-bottom: 15px;
+`;
+
+export const SubmitBtn = styled(Button)`
+  margin-top: 30px;
 `;
