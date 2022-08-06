dca99c3610de9a3420df939033756dc93f1a4844
chmini <papa00103@naver.com>
2022-08-06 17:00:59 +0900

    refactor: 컴포넌트 MatchRecordChart 리팩터링

---
+++ src/components/MatchRecordChart/MatchRecordChart.tsx
@@ -0,0 +1,35 @@
+import dynamic from 'next/dynamic';
+
+import { MatchRecord } from '@interface/team';
+
+import * as S from './MatchRecordChart.styles';
+
+interface Props {
+  matchRecord: MatchRecord;
+}
+
+const PieChart = dynamic(() => import('../PieChart/PieChart'), {
+  ssr: false,
+});
+
+const MatchRecordToPieData = (matchRecord: MatchRecord) =>
+  Object.entries(matchRecord).map(([type, value]: [string, number]) => {
+    switch (type) {
+      case 'win':
+        return { id: '승리', label: `승리 ${value}회`, value };
+      case 'draw':
+        return { id: '무승부', label: `무승부 ${value}회`, value };
+      case 'lose':
+        return { id: '패배', label: `패배 ${value}회`, value };
+      default:
+        return { id: '', label: '', value: 0 };
+    }
+  });
+
+const MatchRecordChart = ({ matchRecord }: Props) => (
+  <S.Wrapper>
+    <PieChart data={MatchRecordToPieData(matchRecord)} />
+  </S.Wrapper>
+);
+
+export default MatchRecordChart;
---
+++ src/components/MatchRecordChart/index.ts
@@ -0,0 +1,1 @@
+export { default as MatchRecordChart } from './MatchRecordChart';
