e0e6613d23d0480e0955d8a976e18b95ed3abad6
Kyubum Jang <jkb2221@gmail.com>
2022-08-06 16:58:52 +0900

    style: Container 삭제 align  어긋나는 문제 해결

--- src/components/ChatReceiver/ChatReceiver.tsx
+++ src/components/ChatReceiver/ChatReceiver.tsx
@@ -7,17 +7,15 @@ const ChatReceiver = () => {
   console.log('');
   // TODO: API 연동한 데이터 props로 넘겨줄 것
   return (
-    <S.Container>
-      <InnerWrapper>
-        <CgProfile size='42px' />
-        <S.ReceiverWrapper>
-          <B3>정기홍 님이 대화를 수락했어요!! 대화를 시작해보세요!</B3>
-        </S.ReceiverWrapper>
-        <S.TimeWrapper>
-          <GrayB3>오전 10:10</GrayB3>
-        </S.TimeWrapper>
-      </InnerWrapper>
-    </S.Container>
+    <InnerWrapper>
+      <CgProfile size='42px' />
+      <S.ReceiverWrapper>
+        <B3>정기홍 님이 대화를 수락했어요!! 대화를 시작해보세요!</B3>
+      </S.ReceiverWrapper>
+      <S.TimeWrapper>
+        <GrayB3>오전 10:10</GrayB3>
+      </S.TimeWrapper>
+    </InnerWrapper>
   );
 };
 
