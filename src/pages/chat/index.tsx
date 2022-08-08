6f9a39e6b65370095344197fc726de719eae239b
Kyubum Jang <jkb2221@gmail.com>
2022-08-08 18:06:15 +0900

    feat: 채팅 전체 목록 API 연동(테스트 필요)

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,48 +1,51 @@
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
+import { axiosAuthInstance } from '@api/axiosInstances';
 import { ChatListItem } from '@components/ChatListItem';
+import { Response } from '@interface/response';
 import { ColWrapper, Container } from '@styles/common';
 
-const DummyData = {
-  chats: [
-    {
-      id: 1, // 매칭 요청 ID (match_proposal_id)
-      content: '저랑 해주세요!', // 매칭 요청 내용 (match_proposal - content)
-      target: {
-        // 채팅 상대
-        nickname: '연승연', // 채팅 상대 닉네임
-      },
-      lastChat: {
-        // 마지막 채팅
-        content: '경기 관련 문의드려요!', // 채팅 내용
-      },
-    },
-    {
-      id: 2, // 매칭 요청 ID (match_proposal_id)
-      content: '나랑 해요', // 매칭 요청 내용 (match_proposal - content)
-      target: {
-        // 채팅 상대
-        nickname: '정기쓰', // 채팅 상대 닉네임
-      },
-      lastChat: {
-        // 마지막 채팅
-        content: '저랑 하시죠.', // 채팅 내용
-      },
-    },
-  ],
+interface Chat {
+  id: number;
+  content: string;
+  target: {
+    nickname: string;
+  };
+  lastChat: {
+    content: string;
+  };
+}
+const ChatListPage: NextPage = () => {
+  const [chatList, setChatList] = useState<Chat[]>([]);
+
+  // useEffect(() => {
+  //   if (!router.isReady) return;
+  //   const ChatListApi = async () => {
+  //     await axiosAuthInstance
+  //       .get<Response<Chat>>('/api/matches/proposals/')
+  //       .then((res) => {
+  //         if (res.status === 200) {
+  //           setChatList(res.data.data);
+  //         }
+  //       });
+  //   };
+  //   ChatListApi();
+  // }, [router.isReady]);
+
+  return (
+    <Container>
+      <ColWrapper gap='16px'>
+        {chatList.map((chat) => (
+          <ChatListItem
+            nickname={chat.target.nickname}
+            lastChat={chat.lastChat.content}
+            key={chat.id}
+          />
+        ))}
+      </ColWrapper>
+    </Container>
+  );
 };
-const ChatListPage: NextPage = () => (
-  <Container>
-    <ColWrapper gap='16px'>
-      {DummyData.chats.map((chat) => (
-        <ChatListItem
-          nickname={chat.target.nickname}
-          lastChat={chat.lastChat.content}
-          key={chat.id}
-        />
-      ))}
-    </ColWrapper>
-  </Container>
-);
 
 export default ChatListPage;
