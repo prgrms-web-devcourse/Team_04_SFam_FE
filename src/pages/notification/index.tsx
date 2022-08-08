73d7c58d4b9a1e10ea02d5fc24588e85f97cfe2d
qq8721443 <qq8721443@naver.com>
2022-08-08 23:58:31 +0900

    feat: 초대 목록 무한 스크롤 구현

--- src/pages/notification/index.tsx
+++ src/pages/notification/index.tsx
@@ -1,62 +1,109 @@
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
+import { useEffect, useRef, useState } from 'react';
 
 import { axiosAuthInstance } from '@api/axiosInstances';
 import { NotificationListItem } from '@components/NotificationListItem';
 import { Response } from '@interface/response';
-import { userState } from '@recoil/atoms';
 import { BoldB1, ColWrapper, Container } from '@styles/common';
 
 interface Invitation {
   values: [];
+  hasNext: boolean;
+  cursor: {
+    createdAt: string;
+    id: number;
+  };
 }
 
 interface TeamInvite {
   createdAt: string;
   invitationId: number;
   name: string;
-  teamId: number;
 }
 
 const NotificationPage: NextPage = () => {
-  const router = useRouter();
-  const [loginUser] = useRecoilState(userState);
-  const [invitations, setInvitations] = useState<Invitation>();
+  const [state, setState] = useState({
+    values: [],
+    hasNext: false,
+    cursor: {
+      createdAt: '',
+      id: 0,
+    },
+  });
+  const [isLoading, setIsLoading] = useState(true);
+  const observerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!router.isReady) return;
-
-    const InvitationListApi = async () => {
-      await axiosAuthInstance
-        .get<Response<Invitation>>('/api/teams/invitations', {
-          params: {
-            // createdAt: ,
-            // id: ,
-            size: 10,
-            status: 'WAITING',
-          },
-        })
-        .then((res) => {
-          if (res.status === 200) {
-            setInvitations(res.data.data);
-          }
+    const getInvitationList = async () => {
+      const res = await axiosAuthInstance.get<Response<Invitation>>('/api/teams/invitations', {
+        params: {
+          size: 10,
+          status: 'WAITING',
+        },
+      });
+      if (res.status === 200) {
+        setState({
+          values: res.data.data.values,
+          hasNext: res.data.data.hasNext,
+          cursor: res.data.data.cursor,
         });
+        setIsLoading(false);
+      }
     };
-    InvitationListApi();
-  }, [loginUser.id, router.isReady]);
+    getInvitationList();
+  }, []);
+
+  const getMoreInvitationList = async () => {
+    const res = await axiosAuthInstance.get<Response<Invitation>>('/api/teams/invitations', {
+      params: {
+        createdAt: state.cursor.createdAt,
+        id: state.cursor.id,
+        size: 10,
+        status: 'WAITING',
+      },
+    });
+    setState({
+      ...state,
+      cursor: res.data.data.cursor,
+      values: [...state.values, ...res.data.data.values],
+      hasNext: res.data.data.hasNext,
+    });
+  };
+
+  useEffect(() => {
+    if (state.values?.length && observerRef.current !== null && state.hasNext) {
+      const lastItem = observerRef.current.children[observerRef.current.children.length - 1];
+      const io = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (!isLoading && entry.isIntersecting) {
+              getMoreInvitationList();
+              io.unobserve(lastItem);
+            }
+          });
+        },
+        { threshold: 1 },
+      );
+      io.observe(lastItem);
+    }
+  }, [state.values?.length]);
 
   return (
     <Container>
       <ColWrapper gap='16px'>
-        {invitations ? (
-          invitations.values.map((invitation: TeamInvite) => (
-            <NotificationListItem
-              name={invitation.name}
-              key={invitation.teamId}
-            />
-          ))
+        {state ? (
+          <ColWrapper
+            ref={observerRef}
+            gap='16px'
+          >
+            {state.values.map((invitation: TeamInvite) => (
+              <NotificationListItem
+                name={invitation.name}
+                invitationId={invitation.invitationId}
+                key={invitation.invitationId}
+              />
+            ))}
+          </ColWrapper>
         ) : (
           <BoldB1>알림이 없습니다!</BoldB1>
         )}
