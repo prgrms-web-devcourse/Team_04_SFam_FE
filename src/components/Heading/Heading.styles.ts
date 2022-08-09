c891e425355e3c8ac4001f08adafb57c83c2bc30
chmini <papa00103@naver.com>
2022-08-09 11:29:56 +0900

    feat: 메인 페이지 Header 변경

--- src/components/Heading/Heading.styles.ts
+++ src/components/Heading/Heading.styles.ts
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
 
-export const HeadingWrapper = styled.div`
+export const HeadingContainer = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
   position: fixed;
   height: 70px;
@@ -10,11 +11,17 @@ export const HeadingWrapper = styled.div`
   right: 0;
   padding: 1rem;
   background-color: ${(props) => props.theme.color.background};
-  border-bottom: 1px solid ${(props) => props.theme.color.gray400};
+  border-bottom: 1px solid ${(props) => props.theme.color.gray200};
   z-index: 1;
 `;
 
 export const HeadingTitle = styled.h1`
   font-size: ${(props) => props.theme.fontSize.b1};
   font-weight: bold;
+`;
+
+export const HeadingLinkContainer = styled.div`
+  display: flex;
+  gap: 4px;
+  font-size: ${({ theme }) => theme.fontSize.b3};
 `;
