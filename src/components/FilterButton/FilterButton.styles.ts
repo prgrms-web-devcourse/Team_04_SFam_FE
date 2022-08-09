280b27c377ab780c3458ae10e74f5cb8a582832a
chmini <papa00103@naver.com>
2022-08-09 11:29:08 +0900

    feat: 컴포넌트 FilterButton 스타일 변경

--- src/components/FilterButton/FilterButton.styles.ts
+++ src/components/FilterButton/FilterButton.styles.ts
@@ -1,22 +1,19 @@
 import styled from '@emotion/styled';
 
-interface Props {
-  color: string;
-  size: string;
-  fontSize: string;
-  fontColor: string;
-  margin: string;
+export interface StyleProps {
+  active?: boolean;
 }
 
-export const Container = styled.button<Props>`
-  position: relative;
-  display: inline-block;
-  border: none;
-  border-radius: 50%;
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
-  font-size: ${({ fontSize }) => fontSize};
-  margin-right: ${({ margin }) => (margin === 'default' ? '0' : margin)};
-  background-color: ${({ color, theme }) => (color === 'primary' ? `${theme.color.green200}` : color)};
-  color: ${({ fontColor, theme }) => (fontColor === 'primary' ? `${theme.color.background}` : fontColor)};
-`;
+export const FilterButton = styled('button')<StyleProps>(
+  {
+    outline: 'none',
+    padding: '4px 12px',
+  },
+  ({ theme, active }) => ({
+    border: active ? 'none' : `1px solid ${theme.color.gray200}`,
+    fontSize: theme.fontSize.b3,
+    backgroundColor: active ? theme.color.secondary : theme.color.background,
+    color: active ? '#fff' : '#000',
+    borderRadius: theme.borderRadius,
+  }),
+);
--- src/components/FilterButton/FilterButton.tsx
+++ src/components/FilterButton/FilterButton.tsx
@@ -1,36 +1,19 @@
+import React from 'react';
 import * as S from './FilterButton.styles';
 
-interface Props {
-  color?: string;
-  size?: string;
-  fontSize?: string;
-  fontColor?: string;
-  margin?: string;
-  children?: string;
+interface Props extends S.StyleProps {
+  children: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const FilterButton = ({
-  color = 'primary',
-  size = '100px',
-  fontSize = '20px',
-  fontColor = 'primary',
-  margin = 'default',
-  onClick,
-  children,
-  ...props
-}: Props) => (
-  <S.Container
-    color={color}
-    size={size}
-    fontSize={fontSize}
-    fontColor={fontColor}
+const FilterButton = ({ onClick, children, active, ...props }: Props) => (
+  <S.FilterButton
+    active={active}
     onClick={onClick}
-    margin={margin}
     {...props}
   >
     {children}
-  </S.Container>
+  </S.FilterButton>
 );
 
 export default FilterButton;
