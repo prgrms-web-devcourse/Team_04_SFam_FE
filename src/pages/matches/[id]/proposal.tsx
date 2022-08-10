db9e4d8ff76b81448cf335cc67e8f34f367199cd
dustmddus <dustmddus5@gmail.com>
2022-08-11 02:26:01 +0900

    refactor: 대결 공고 신청 페이지 버그 수정

--- src/pages/matches/[id]/proposal.tsx
+++ src/pages/matches/[id]/proposal.tsx
@@ -1,14 +1,14 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { axiosAuthInstance } from '@api/axiosInstances';
 import { Button } from '@components/Button';
 import { Dropdown, Item } from '@components/Dropdown';
 import { Match } from '@interface/match';
 import { Response } from '@interface/response';
+import { TeamInfo, Team } from '@interface/team';
 import { Container, ColWrapper, TextArea } from '@styles/common';
-import { Team } from 'interface/team';
 
 const changeTeamsToDropdownItems = (teams: Team[]) =>
   teams.map(({ id, name, sportsCategory }, idx) => ({ id: idx, text: name, value: { id, sportsCategory } }));
@@ -20,6 +20,24 @@ const Proposal: NextPage = () => {
   const [teams, setTeams] = useState<Team[]>([]);
   const [matchInfo, setMatchInfo] = useState<Match>();
   const [proposalData, setProposalData] = useState({ teamId: 0, content: '' });
+
+  const [teamInfo, setTeamInfo] = React.useState<TeamInfo>({
+    name: '',
+    description: '',
+    sportsCategory: '',
+    leader: {},
+    members: [],
+    matchRecord: {
+      win: 0,
+      draw: 0,
+      lose: 0,
+    },
+    matchReview: {
+      bestCount: 0,
+      likeCount: 0,
+      dislikeCount: 0,
+    },
+  });
 
   useEffect(() => {
     (async () => {
@@ -43,6 +61,10 @@ const Proposal: NextPage = () => {
     getMatchInfoApi();
   }, [id, router.isReady]);
 
+  const handleSelect = (item: Item<{ id: number; sportsCategory: string }>) => {
+    setProposalData({ ...proposalData, teamId: item.value.id });
+  };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setProposalData(() => ({ ...proposalData, [name]: value }));
@@ -50,28 +72,45 @@ const Proposal: NextPage = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (matchInfo?.matchType === 'TEAM_MATCH' && proposalData.teamId === 0) {
+      alert('팀을 선택하세요!');
+      return;
+    }
+
     (async () => {
-      const res = await axiosAuthInstance({
-        method: 'POST',
-        url: `/api/matches/${id as string}/proposals`,
-        data: proposalData,
-      });
+      const {
+        data: { data },
+      } = await axiosAuthInstance.get<Response<TeamInfo>>(`/api/teams/${proposalData.teamId}`);
 
-      try {
-        if (res.status === 200) {
-          // TODO: 토스트 나중에 띄울 것
-          alert(`[${matchInfo?.title as string}] 공고에 신청되었습니다!`);
-          // TODO: 공고 신청 후에 매치 상세로 넘어가면 공고 상세 출력 안되는 버그 발생({"code":"C0004","message":"Runtime error"})
-          router.push(`/matches/${id as string}`);
-        }
-      } catch (error) {
-        console.log(error);
-      }
+      setTeamInfo(() => data);
     })();
-  };
 
-  const handleSelect = (item: Item<{ id: number; sportsCategory: string }>) => {
-    setProposalData({ ...proposalData, teamId: item.value.id });
+    console.log(matchInfo?.participants);
+    console.log(teamInfo.members.length);
+
+    if (matchInfo?.matchType === 'TEAM_MATCH' && matchInfo?.participants !== teamInfo.members.length) {
+      alert('참여 인원이 맞지 않습니다!');
+    } else {
+      (async () => {
+        const res = await axiosAuthInstance({
+          method: 'POST',
+          url: `/api/matches/${id as string}/proposals`,
+          data: proposalData,
+        });
+
+        try {
+          if (res.status === 200) {
+            // TODO: 토스트 나중에 띄울 것
+            alert(`[${matchInfo?.title as string}] 공고에 신청되었습니다!`);
+            // TODO: 공고 신청 후에 매치 상세로 넘어가면 공고 상세 출력 안되는 버그 발생({"code":"C0004","message":"Runtime error"})
+            router.push(`/matches/${id as string}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
   };
 
   return (
