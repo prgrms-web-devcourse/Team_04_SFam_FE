a0b502a5aed9544295008d6240cfea10924197a7
Kyubum Jang <jkb2221@gmail.com>
2022-08-12 21:40:55 +0900

    feat: 팀 프로필 수정 페이지 구현

---
+++ src/pages/team/[id]/edit.tsx
@@ -0,0 +1,106 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+
+import { axiosAuthInstance } from '@api/axiosInstances';
+import { Avatar } from '@components/Avatar';
+import { Button } from '@components/Button';
+import { Response } from '@interface/response';
+import { TeamInfo } from '@interface/team';
+import { userState } from '@recoil/atoms';
+import { Container, InnerWrapper } from '@styles/common';
+
+interface EditProfile {
+  teamName?: string;
+  logoImageUrl?: string | null;
+}
+
+const UserEditPage: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [user] = useRecoilState(userState);
+
+  const [teamInfo, setTeamInfo] = useState<TeamInfo>();
+  const [isLeader, setIsLeader] = useState<boolean>();
+  const [editTeamProfile, setEditTeamProfile] = useState<EditProfile>({
+    teamName: teamInfo?.name,
+    logoImageUrl: teamInfo?.logoImageUrl,
+  });
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    (async () => {
+      const {
+        data: { data },
+      } = await axiosAuthInstance.get<Response<TeamInfo>>(`/api/teams/${id as string}`);
+      setTeamInfo(() => data);
+      if (data.leader.id === user.id) {
+        setIsLeader(true);
+      }
+    })();
+  }, [router.isReady]);
+
+  const handleClick = () => {
+    router.push(`/team/${id as string}`);
+  };
+
+  const handleTeamFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    const patchTeamProfileAPi = async () => {
+      try {
+        if (!files) {
+          alert('파일이 존재하지 않습니다.');
+        } else {
+          const formData = new FormData();
+          formData.append('file', files[0]);
+          if (teamInfo) {
+            const res = await axiosAuthInstance.patch(`/api/teams/${teamInfo.id}/logo`, formData, {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+              },
+            });
+            alert(`${files[0].name} 팀 로고 이미지가 업로드 되었습니다.`);
+            // TODO: API 수정 이후 확인할 것 res에 이미지 URL 요청드림
+            // setEditTeamProfile({ ...editTeamProfile, teamLogoFile: res.data.data.logoImageUrl });
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    patchTeamProfileAPi();
+  };
+
+  return isLeader ? (
+    <Container>
+      <InnerWrapper
+        margin='20px 0 20px 0'
+        alignItems='center'
+        justifyContent='center'
+      >
+        {user && teamInfo && editTeamProfile.logoImageUrl !== null ? (
+          <Avatar
+            imgSrc={editTeamProfile.logoImageUrl}
+            imgSize='100px'
+            edit
+            team
+            handleFileChange={handleTeamFileChange}
+          />
+        ) : (
+          <Avatar
+            edit
+            team
+            handleFileChange={handleTeamFileChange}
+          />
+        )}
+      </InnerWrapper>
+      <Button onClick={handleClick}>완료</Button>
+    </Container>
+  ) : (
+    <div />
+  );
+};
+
+export default UserEditPage;
