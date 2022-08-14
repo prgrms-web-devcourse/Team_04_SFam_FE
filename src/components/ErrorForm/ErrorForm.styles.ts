f19d23626eeda9b90d0d6084aa215cd7cc85f35e
Kyubum Jang <jkb2221@gmail.com>
2022-08-14 16:52:19 +0900

    feat: ErrorForm 컴포넌트 구현

---
+++ src/components/ErrorForm/ErrorForm.styles.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const ErrorText = styled.span`
+  font-size: ${(props) => props.theme.fontSize.h1};
+  font-weight: bold;
+`;
+
+export const SuggestionText = styled.span`
+  font-size: ${(props) => props.theme.fontSize.b2};
+  font-weight: bold;
+`;
