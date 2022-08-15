650d6574e65ab5107f94eb9bd4047220f160f12c
qq8721443 <qq8721443@naver.com>
2022-08-15 14:34:35 +0900

    feat: 메시지 새로고침 기능 추가

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+
+interface UseInterval {
+  (callback: () => void, interval: number): void;
+}
+
+export const useInterval: UseInterval = (callback, interval) => {
+  const savedCallback = useRef<(() => void) | null>(null);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+
+    const id = setInterval(tick, interval);
+    return () => clearInterval(id);
+  }, [interval]);
+};
